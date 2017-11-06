6758141e01e048596002c9b49eaa00727294096c
spodolsky <33435480+spodolsky@users.noreply.github.com>
2017-11-06 17:42:04 -0500

    Update -.py

--- -.py
+++ -.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 plt.clf()
-image = img.imread("world-map.gif")
-plt.imshow(image, extent = [0, 197.349, -64, 88.809])
 QuakeData = open("currentQuakes.txt")
 QuakeData.readline()
 LAT = []
@@ -12,4 +10,6 @@ for line in QuakeData:
     LAT.append(float(line[1]))
     LONG.append(float(line[2]))
 plt.scatter(LONG, LAT, label = "o")
+image = img.imread("world-map.gif")
+plt.imshow(image, extent = [-197.962, 197.701, -63.8313, 87.9263])
 plt.show()
